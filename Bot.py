3505b55712a93e5c0164f58114a556cd33cae992
jestraogi8-netizen <jestraogi8@gmail.com>
2025-09-21 03:15:57 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,26 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-TOKEN = "8210186257:AAFjuYyNEM78WlEO4knsqmT3j7Sgz7Pu5cs"
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Token will be read from environment variable on Render
+TOKEN = os.getenv("TOKEN")
+
+def start(update, context):
+    update.message.reply_text("Bot is live ✅")
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+print("Bot started... go to @Jftank_bot and type /start")
+updater.start_polling()
+updater.idle()"
 
 def start(update, context):
     update.message.reply_text("Bot is live ✅")
